c011e441df0d9afaf25caa3ea25b83d20040fc12
Stee1Lemon <121914099+Stee1Lemon@users.noreply.github.com>
2024-04-21 18:06:20 +0100

    1

--- src/components/VehicleCard/index.jsx
+++ src/components/VehicleCard/index.jsx
@@ -1,6 +1,6 @@
 import Modal from 'components/Modal';
 import VehicleInfo from 'components/VehicleInfor';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import {
   Card,
   Description,
